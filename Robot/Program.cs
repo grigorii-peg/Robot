4e60723755d3efd1f55c5ab4fbb56a4aa3112814
grigorii-peg <106974779+grigorii-peg@users.noreply.github.com>
2022-09-18 23:13:26 +0300

    Add files via upload

---
+++ Robot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            Robot robick = new Robot();
+            int i = 0;
+
+            Console.WriteLine("Введите путь робота при помощи стрелочек на клавиатуре(W - вверх, S - вниз, A - влево, D - вправо");
+            Console.SetCursorPosition(robick.x, robick.y);
+            Console.CursorVisible = false;
+            ConsoleKeyInfo key;
+            Console.Write((char)(127));
+
+            while (run)
+            {
+                key = Console.ReadKey(true);
+                robick.Shag(ref key, ref robick.x, ref robick.y);
+                run = robick.End();
+                i++;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Вы вернулись.\nВсего шагов: {robick.count}");
+            Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ Robot/Robot.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot
+{
+    internal class Robot
+    {
+        public int x = Console.WindowWidth / 2;
+        public int y = Console.WindowHeight / 2;
+        public int count = 0;
+
+        public void Shag (ref ConsoleKeyInfo key, ref int x, ref int y)
+        {
+            if (key.KeyChar == 119 || key.KeyChar == 87)
+            {
+                Console.SetCursorPosition(x, --y);
+                Console.Write("∙");
+            }
+            else if(key.KeyChar == 115 || key.KeyChar == 83)
+            {
+                Console.SetCursorPosition(x, ++y);
+                Console.Write("∙");
+            }
+            else if (key.KeyChar == 97 || key.KeyChar == 65)
+            {
+                Console.SetCursorPosition(--x, y);
+                Console.Write("∙");
+            }
+            else if (key.KeyChar == 100 || key.KeyChar == 68)
+            {
+                Console.SetCursorPosition(++x, y);
+                Console.Write("∙");
+            }
+            count++;
+            
+        }   
+        public bool End()
+        {
+            if(x == (Console.WindowWidth / 2) && y == (Console.WindowHeight / 2) || count > 100)
+            {
+                if (count > 100)
+                {
+                    Console.WriteLine("Не пришли");
+                }
+                return false;
+
+            }
+            return true;
+        }
+    }
+}
